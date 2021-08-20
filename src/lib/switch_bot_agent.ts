5c73dcf1f663d134556931bf77d5712b5ff9e193
bowzstandard <bowz@bowzstandard.net>
2021-08-20 09:28:42 +0900

    debug

--- src/lib/switch_bot_agent.ts
+++ src/lib/switch_bot_agent.ts
@@ -2,24 +2,29 @@ import Switchbot from 'node-switchbot';
 
 class SwitchbotAgentImpl {
   async scanAndPress(deviceAddress: string) {
-    const switchBot = new Switchbot();
-    const bot_list = await switchBot.discover({
-      model: 'H',
-      quick: false,
-      id: deviceAddress,
-    });
+    try {
+      const switchBot = new Switchbot();
+      const bot_list = await switchBot.discover({
+        model: 'H',
+        quick: false,
+        id: deviceAddress,
+      });
 
-    if (bot_list.length === 0) {
-      throw new Error('No device was found.');
+      if (bot_list.length === 0) {
+        throw new Error('No device was found.');
+      }
+      console.log(bot_list);
+      // The `SwitchbotDeviceWoHand` object representing the found Bot.
+      const device = bot_list[0];
+      // Put the Bot's arm down (stretch the arm)
+      await device.down();
+      // Wait for 5 seconds
+      // await switchBot.wait(5000);
+      // Put the Bot's arm up (retract the arm)
+      await device.up();
+    } catch (e) {
+      console.log('SWICHBOT ERROR', e);
     }
-    // The `SwitchbotDeviceWoHand` object representing the found Bot.
-    const device = bot_list[0];
-    // Put the Bot's arm down (stretch the arm)
-    await device.down();
-    // Wait for 5 seconds
-    // await switchBot.wait(5000);
-    // Put the Bot's arm up (retract the arm)
-    await device.up();
   }
 }
 
