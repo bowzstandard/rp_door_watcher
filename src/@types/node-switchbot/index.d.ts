f6a3f45438e541f8a68630057fec0c78db15e198
bowzstandard <bowz@bowzstandard.net>
2021-11-03 22:42:30 +0900

    fix

--- src/@types/node-switchbot/index.d.ts
+++ src/@types/node-switchbot/index.d.ts
@@ -1,25 +1,23 @@
 declare module 'node-switchbot' {
-  namespace Switchbot {
-    class Switchbot {
-      discover: (
-        params: ISwitchbotDiscoverParams
-      ) => Promise<SwitchbotDevice[]>;
-      wait: (msec: number) => Promise<void>;
-    }
-    class SwitchbotDevice {
-      down: () => Promise<void>;
-      up: () => Promise<void>;
-      press: () => Promise<void>;
-      id: string;
-      address: string;
-      model: string;
-    }
-    type ISwitchbotDiscoverParams = {
-      duration?: number;
-      model?: 'H' | 'T' | 'M' | 'CS' | 'c';
-      id?: string;
-      quick?: boolean;
-    };
+  class Switchbot {
+    discover: (params: ISwitchbotDiscoverParams) => Promise<SwitchbotDevice[]>;
+    wait: (msec: number) => Promise<void>;
   }
-  export = Switchbot;
+
+  export type SwitchbotDevice = {
+    down: () => Promise<void>;
+    up: () => Promise<void>;
+    press: () => Promise<void>;
+    id: string;
+    address: string;
+    model: string;
+  };
+
+  export type ISwitchbotDiscoverParams = {
+    duration?: number;
+    model?: 'H' | 'T' | 'M' | 'CS' | 'c';
+    id?: string;
+    quick?: boolean;
+  };
+  export default Switchbot;
 }
